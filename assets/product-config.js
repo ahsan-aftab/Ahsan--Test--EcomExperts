8ae223516ab83e544cc5a81ebb577ef203295645
Ahsan Aftab <Ahsan.aftab99@gmail.com>
2023-04-08 00:04:49 +0500

    minor update

--- assets/product-config.js
+++ assets/product-config.js
@@ -23,26 +23,23 @@ document.addEventListener("DOMContentLoaded", function(){
 // On add to cart button click check and send ATC request to Shopify  
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button[name=add]').addEventListener('click', function(event) {
-        setTimeout(()=>{
-            if (document.querySelector('[name=id]').value == '44970625073464') {
-                fetch('/cart/add.js', {
-                  method: 'POST',
-                  headers: {
-                    'Content-Type': 'application/json'
-                  },
-                  body: JSON.stringify({
-                    quantity: 1,
-                    id: 44964875731256,
-                    sections: 'cart-notification-product,cart-notification-button,cart-icon-bubble',
-                    sections_url: '/products/dark-winter-jacket'
-                  })
-                }).then(response => {
-                  // handle response
-                }).catch(error => {
-                  // handle error
-                });
-              }
-        },100);  
-        
+          if (document.querySelector('[name=id]').value == '44970625073464') {
+              fetch('/cart/add.js', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  quantity: 1,
+                  id: 44964875731256,
+                  sections: 'cart-notification-product,cart-notification-button,cart-icon-bubble',
+                  sections_url: '/products/dark-winter-jacket'
+                })
+              }).then(response => {
+                // handle response
+              }).catch(error => {
+                // handle error
+              });
+            }
     });
   });
