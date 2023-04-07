02693f42bcf750b7c5ffd21f29312b8a6fa4726c
Ahsan Aftab <Ahsan.aftab99@gmail.com>
2023-04-08 00:39:21 +0500

    test check-in

--- assets/product-config.js
+++ assets/product-config.js
@@ -23,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function(){
 // On add to cart button click check and send ATC request to Shopify  
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button[name=add]').addEventListener('click', function(event) {
+      debugger;
             if (document.querySelector('[name=id]').value == '44970625073464') {
                 fetch('/cart/add.js', {
                   method: 'POST',
@@ -34,6 +35,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     id: 44964875731256,
                   })
                 }).then(response => {
+                  console.log('res', response)
+                  debugger;
                   // handle response
                 }).catch(error => {
                   // handle error
