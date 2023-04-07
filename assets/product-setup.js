ca606103b76cefd30fadcc9602f1373bd7891b44
Ahsan Aftab <Ahsan.aftab99@gmail.com>
2023-04-07 17:44:37 +0500

    add color swatch and other updates

---
+++ assets/product-setup.js
@@ -0,0 +1,33 @@
+let url = window.location.href;
+if(url.includes('variant')){
+    window.location.href = window.location.origin + window.location.pathname;
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    let sizeDOMElem =document.querySelector('select[name="options[Size]"]');
+    if(sizeDOMElem && sizeDOMElem.options.length > 0){
+        sizeDOMElem.options[0].selected = true
+        document.querySelectorAll('variant-selects')[0].onVariantChange()
+        sizeDOMElem.options[0].innerHTML = sizeDOMElem.options[0].innerHTML.replace('- Unavailable',"");
+    } 
+
+
+
+    let colorsCheckBox = document.querySelectorAll('input[type=radio][name=Color]');
+    if(colorsCheckBox){
+        for(let i = 0; i < colorsCheckBox.length; i++){
+            colorsCheckBox[i].addEventListener('change', function(e){
+                let color = e.target.value;
+                let colorDropDownElem = document.querySelectorAll('select[name="options[Color]"]')[0];
+                let colorOptionElems = colorDropDownElem.options;
+                for(let j = 0; j < colorOptionElems.length; j++){
+                    if(colorOptionElems[j].value == color){
+                        document.querySelector('select[name="options[Color]"]').options[j].selected = true
+                        document.querySelectorAll('variant-selects')[0].onVariantChange()
+                    }
+                }
+            });
+        }
+    }
+
+}); // End DOMContentLoaded 
