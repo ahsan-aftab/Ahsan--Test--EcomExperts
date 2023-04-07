1975330bcad20aefaf72a331a4fee1afb585d990
Ahsan Aftab <Ahsan.aftab99@gmail.com>
2023-04-07 23:52:08 +0500

    minor update

--- assets/product-config.js
+++ assets/product-config.js
@@ -30,9 +30,8 @@ document.addEventListener("DOMContentLoaded", function(){
 // On add to cart button click check and send ATC request to Shopify  
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('button[name=add]').addEventListener('click', function(event) {
-        setTimeout(function(){
           if (document.querySelector('[name=id]').value == '44970625073464') {
-            fetch('/cart/add', {
+            fetch('/cart/add.js', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json'
@@ -40,6 +39,8 @@ document.addEventListener('DOMContentLoaded', function() {
               body: JSON.stringify({
                 quantity: 1,
                 id: 44964875731256,
+                sections: 'cart-notification-product,cart-notification-button,cart-icon-bubble',
+                sections_url: '/products/dark-winter-jacket'
               })
             }).then(response => {
               // handle response
@@ -47,7 +48,5 @@ document.addEventListener('DOMContentLoaded', function() {
               // handle error
             });
           }
-        }, 100);
-        
     });
   });
