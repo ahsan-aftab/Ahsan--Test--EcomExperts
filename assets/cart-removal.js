d599adc928778dd419dc091e63274dcb0942d0ad
Ahsan Aftab <Ahsan.aftab99@gmail.com>
2023-04-08 00:13:44 +0500

    minor update

--- assets/cart-removal.js
+++ assets/cart-removal.js
@@ -3,16 +3,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let removeButtons = document.querySelectorAll('cart-remove-button a');
     for(let i = 0; i < removeButtons.length; i++ ){
         removeButtons[i].addEventListener('click', function(e){
-            debugger;
             let a = e.currentTarget;
             if (a.href.includes('id=44970625073464')) {
+                e.preventDefault();
+
                 fetch('/cart/update.js', {
                     method: 'POST',
                     headers: {
                     'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({ updates:{
-                    44964875731256: 0}
+                    44964875731256: 0,
+                    44970625073464:0}
                     })
                 }).then(response => {
                     setTimeout(()=>{
